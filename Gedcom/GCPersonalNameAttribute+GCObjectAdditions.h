da2e106cd4389593fe5dce5d130d20f8eae2e95d
Al Zonke <al.zonke@yahoo.com>
2013-06-10 11:48:53 +0300

    Support 5.5.1. version of file storage.

---
+++ Gedcom/GCPersonalNameAttribute+GCObjectAdditions.h
@@ -0,0 +1,16 @@
+//
+//  GCPersonalNameAttribute+GCObjectAddition.h
+//  Gedcom
+//
+//  Created by Alexey Lizenko on 5/31/13.
+//  Copyright (c) 2013 Mikkel Eide Eriksen. All rights reserved.
+//
+// ********************************************************************************************************************************************************** //
+
+#import <Gedcom/Gedcom.h>
+
+// ********************************************************************************************************************************************************** //
+
+@interface GCPersonalNameAttribute (GCObjectAddition)
+
+@end
---
+++ Gedcom/GCPersonalNameAttribute+GCObjectAdditions.m
@@ -0,0 +1,16 @@
+//
+//  GCPersonalNameAttribute+GCObjectAddition.m
+//  Gedcom
+//
+//  Created by Alexey Lizenko on 5/31/13.
+//  Copyright (c) 2013 Mikkel Eide Eriksen. All rights reserved.
+//
+// ********************************************************************************************************************************************************** //
+
+#import "GCPersonalNameAttribute+GCObjectAddition.h"
+
+// ********************************************************************************************************************************************************** //
+
+@implementation GCPersonalNameAttribute (GCObjectAddition)
+
+@end
